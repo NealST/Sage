4f5345a2bcba50ae167da429eaa0efe7ef63f7cb
NealST <m13710224694@163.com>
2026-07-11 11:11:52 +0800

    Initial Commit

---
+++ Sage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sage
+//
+//  Created by Neal Kaffrey on 2026/7/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sage/SageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SageApp.swift
+//  Sage
+//
+//  Created by Neal Kaffrey on 2026/7/11.
+//
+
+import SwiftUI
+
+@main
+struct SageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
